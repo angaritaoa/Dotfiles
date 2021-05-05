cae0e3251578928f64ac85ccebb01aa5d38f903f
Andres Angarita <angaritaoa@gmail.com>
2021-05-04 21:51:43 -0500

    Organizando el repositorio

---
+++ install.zsh
@@ -0,0 +1,27 @@
+# Terminal Emulator: Terminator
+~/.config/terminator/config
+
+# Ruta para las fuentes
+/usr/share/fonts
+
+# Remmina
+~/.local/share/remmina
+
+# Xresources
+~/.Xresources
+
+# gitconfig
+~/.gitconfig
+
+# zshrc
+~/.zshrc
+
+# p10k.zsh
+~/.p10k.zsh
+
+# local.conf
+/etc/fonts/local.conf
+
+# Xorg
+/etc/X11/xorg.conf
+
